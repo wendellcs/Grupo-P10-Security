4dd31e785d8a4b5aca5a895b727c3817b051b927
Wendell <wendellcs131@gmail.com>
2024-11-27 03:28:03 -0300

    Add services page

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -1,7 +1,49 @@
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import '../../assets/sass/layouts/_services.sass'
+
+import services from './servicesData.js'
+import { useState } from 'react'
+
+import { RiArrowDropDownLine } from "react-icons/ri";
+
 export default function Services(){
+    const [serviceToBeShown, setServiceToBeShown] = useState(0)
+
     return (
-        <div>
-            <p>services page</p>
+        <div className='container'>
+            <Header />
+            <main className='services-page'>
+                <h2 className="section-title">Nossos serviços</h2>
+
+                <div className="services-page-content">
+                    <h3 className="block-title">Explore nossos serviços</h3>
+
+                    <div className="services-page-content-services">
+                        {services.map((service) => {
+                            return (
+                                <div className='service-box' key={service.id} onClick={() => setServiceToBeShown(service.id)}>
+                                    <div className='title-container'>
+                                        <h3 className="service-box-title">{service.name}</h3>
+                                        <RiArrowDropDownLine className={serviceToBeShown === service.id ? "arrow active": "arrow"} />
+                                    </div>
+                                    <div className={serviceToBeShown === service.id ? "wrapper active": "wrapper"}>
+                                        <div className="expandable">
+                                            <p className="service-subtitle">Sobre o serviço</p>
+                                            <p className='text service'>{service.description}</p>
+
+                                            <p className="service-subtitle">Como funciona?</p>
+                                            <p className='text service'>{service.process}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+
+            </main>
+            <Footer />
         </div>
     )
 }
--- src/pages/Services/servicesData.js
+++ src/pages/Services/servicesData.js
@@ -1,25 +1,43 @@
-export const services = [{
+const services = [{
         name: 'Vigilância Patrimonial',
-        description: 'Description of Service 1',
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        process: 'Lorem ipsum dolor sit amet, consectetur adip iscing elit sed diam nonumy eirmod tempor incidid pretium et justo just another lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        id: 0
     },{
         name: 'Motorista profissional',
-        description: 'Description of Service 2',
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        process: 'Lorem ipsum dolor sit amet, consectetur adip iscing elit sed diam nonumy eirmod tempor incidid pretium et justo just another lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        id: 1
     }, {
         name: 'Contraespionagem industrial',
-        description: 'Description of Service 3',
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        process: 'Lorem ipsum dolor sit amet, consectetur adip iscing elit sed diam nonumy eirmod tempor incidid pretium et justo just another lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        id: 2
     },{
         name: 'Projetos de Programa Gestão de Risco',
-        description: 'Description of Service 4',
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        process: 'Lorem ipsum dolor sit amet, consectetur adip iscing elit sed diam nonumy eirmod tempor incidid pretium et justo just another lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        id: 3
     },{
         name: 'Segurança de VIP',
-        description: 'Description of Service 5',
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        process: 'Lorem ipsum dolor sit amet, consectetur adip iscing elit sed diam nonumy eirmod tempor incidid pretium et justo just another lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        id: 4
     },{
         name: 'Segurança cibernpética',
-        description: 'Description of Service 6',
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        process: 'Lorem ipsum dolor sit amet, consectetur adip iscing elit sed diam nonumy eirmod tempor incidid pretium et justo just another lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        id: 5
     },{
         name: 'Escolta Veicular',
-        description: 'Description of Service 7',
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        process: 'Lorem ipsum dolor sit amet, consectetur adip iscing elit sed diam nonumy eirmod tempor incidid pretium et justo just another lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        id: 6
     },{
         name: 'Transporte de valores',
-        description: 'Description of Service 8',
-}]
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        process: 'Lorem ipsum dolor sit amet, consectetur adip iscing elit sed diam nonumy eirmod tempor incidid pretium et justo just another lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam nonumy eirmod tempor incididunt ut labore et',
+        id: 7
+    }]
+
+export default services
